f555993039ffc99b10ee1f00fe92e09f1107a3f7
elieak <eliea@code-college.net>
2016-07-27 22:36:50 +0300

    Add files via upload

---
+++ Lab 1.1/Lab 1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_1._1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string fName = "Elie";
+            const string lName = "Karam";
+
+            Console.WriteLine("Hello, C#");
+            Console.WriteLine($"Hello, {fName} {lName}.");
+        }
+    }
+}
+
