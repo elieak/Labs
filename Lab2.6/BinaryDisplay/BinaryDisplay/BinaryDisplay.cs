5ed07484b98fedce74b09833ddbc3f77957f06d7
elieak <eliea@code-college.net>
2016-07-27 22:39:16 +0300

    Add files via upload

---
+++ Lab2.6/BinaryDisplay/BinaryDisplay/BinaryDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryDisplay
+{
+    class BinaryDisplay
+    {
+        static void Main()
+        {
+            int input;
+            var tb = new BinaryDisplay();
+
+            Console.Write("Enter an integer to convert to Binary: ");
+            int.TryParse(Console.ReadLine(), out input);
+            var bin = Convert.ToString(input, 2);
+
+            Console.WriteLine("Binary number representation: " + bin);
+
+            if (input > 0)
+            {
+                Console.WriteLine("Positive int binary count: " + tb.ToBinary(input));
+            }
+            else
+            {
+                Console.WriteLine("Negative int binary count: " + tb.ToNegativeBinary(bin));
+            }
+
+        }
+
+        public double ToBinary(int value)
+        {
+            var a = value;
+            var count = 0;
+
+            while (a > 0)
+            {
+                if ((a & 1) == 1)
+                    count++;
+                var orig = a >> 1;
+                a = orig;
+            }
+            return count;
+        }
+
+        public string ToNegativeBinary(string value)
+        {
+            var arr = value.ToCharArray();
+            var counter = arr.Count(one => one == '1');
+            return counter.ToString();
+        }
+    }
+}
---
+++ Lab2.7/DollarStairs/DollarStairs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DollarStairs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int usrInput;
+            const char dollar = '$';
+            Console.Write("Enter number: ");
+            int.TryParse(Console.ReadLine(), out usrInput);
+
+            for (var i = 0; i < usrInput; i++)
+            {
+                for (var j = 0; j <= i; j++)
+                    Console.Write(dollar);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
