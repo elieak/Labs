df605144a9e3c4c02f14a5b1ae1874cd6a71b95f
Eyal Perry <e.perry.pa@gmail.com>
2016-07-30 11:16:14 +0300

    10.2

--- FileFinder/FileFinder/Program.cs
+++ FileFinder/FileFinder/Program.cs
@@ -8,6 +8,7 @@ namespace FileFinder
     {
         static void Main(string[] args)
         {
+            //Bug: This only searches the file names, it was intended that you search the file contents..
             var directory = Directory.GetFiles(args[0]).Where(file => file.Contains(args[1]));
             foreach (var fileName in directory)
             {
