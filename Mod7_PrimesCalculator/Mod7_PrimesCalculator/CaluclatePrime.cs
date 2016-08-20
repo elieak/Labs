604618922a1bc7feacf6fd6b4e1349619855c29f
elieak <eliea@code-college.net>
2016-08-20 20:48:04 +0300

    Add files via upload

---
+++ Mod7_PrimesCalculator/Mod7_PrimesCalculator/CaluclatePrime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod7_PrimesCalculator
+{
+    public static class CaluclatePrime
+    {public static bool isPrime(this int number)
+        {
+            if (number % 2 == 0)
+                return false;
+            for (int i = 3; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Mod7_PrimesCalculator/Mod7_PrimesCalculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Mod7_PrimesCalculator
+{
+    public partial class Form1 : Form
+    {
+        private int number1 { get; set; }
+        private int number2 { get; set; }
+        private Thread _calculateThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number1 = int.Parse(textBox1.Text);
+                number2 = int.Parse(textBox2.Text);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Please enter a number between 0 and {int.MaxValue}");
+            }
+
+            listBox1.Items.Clear();
+            button1.Enabled = false;
+            _calculateThread = new Thread(() => { CalculatePrimes(number1, number2); });
+            _calculateThread.Start();
+        }
+
+        private void CalculatePrimes(int firstNumber, int secondNumber)
+        {
+            var calculationResult = new List<int>();
+            if (secondNumber < 2 || secondNumber < firstNumber) return;
+
+            if (firstNumber < 3)
+            {
+                calculationResult.Add(2);
+                firstNumber = 3;
+            }
+
+            if (firstNumber%2 == 0)
+            {
+                firstNumber++;
+            }
+                
+            for (var i = firstNumber; i < secondNumber; i += 2)
+            {
+                if (i.isPrime())
+                {
+                    calculationResult.Add(i);
+                }
+            }
+            foreach (var prime in calculationResult)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    listBox1.Items.Add(prime);
+                    button1.Enabled = true;
+                });
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _calculateThread.Abort();
+            button1.Enabled = true;
+        }
+    }
+}
